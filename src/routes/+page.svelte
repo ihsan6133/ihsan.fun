539d73bc74f5d7d8ca08a6e049fe5071bac77560
ihsan6133 <mihsan6133@gmail.com>
2026-05-15 23:45:03 +0400

    Tested layout and copyright notice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,14 +42,14 @@
 <svg class="block w-full h-12 -mt-12" viewBox="0 0 1440 320" preserveAspectRatio="none">
   <path fill="rgb(118 81 36)" d="M0,160L48,176C96,192,192,224,288,213.3C384,203,480,149,576,144C672,139,768,181,864,181.3C960,181,1056,139,1152,122.7C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
 </svg>
-<div class="h-50 bg-[rgb(118,81,36)]">
+<div class="h-70 bg-[rgb(118,81,36)]">
 
 </div>
 
 <svg class="block w-full h-12 -mt-12 scale-x-[-1]"  viewBox="0 0 1440 320" preserveAspectRatio="none">
   <path fill="rgb(173 136 92)" d="M0,160L48,176C96,192,192,224,288,213.3C384,203,480,149,576,144C672,139,768,181,864,181.3C960,181,1056,139,1152,122.7C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
 </svg>
-<div class="h-30 bg-[rgb(173,136,92)]">
+<div class="h-70 bg-[rgb(173,136,92)]">
 
 </div>
 
@@ -57,15 +57,17 @@
   <path d="M0,30 L120,60 L240,20 L480,80 L720,40 L960,90 L1200,30 L1440,70 L1440,100 L0,100 Z"></path>
 </svg>
 
-<div class="h-30 bg-[rgb(150,108,70)]">
+<div class="h-70 bg-[rgb(150,108,70)]">
 
 </div>
 
 <svg class="w-full h-10 fill-zinc-700 -mt-12" viewBox="0 0 1440 100" preserveAspectRatio="none">
   <path d="M0,50 L300,20 L600,80 L900,40 L1200,70 L1440,30 L1440,100 L0,100 Z"></path>
 </svg>
-<div class="h-30 bg-zinc-700">
-
+<div class="h-50 flex flex-col bg-zinc-700">
+</div>
+<div class="bg-zinc-700 text-center p-2 font-display text-md text-zinc-300 font-bold">
+    Copyright © 2026 Ihsan. All rights reserved.
 </div>
 <!-- 
 <div class="absolute top-0 left-0 w-full h-screen pointer-events-none overflow-hidden" >
