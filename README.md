0db8c8c2705ba8b3d2a493002f860c5ef0a56bd4
ihsan6133 <mihsan6133@gmail.com>
2026-05-16 21:39:21 +0400

    test

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ihsan.fun
 
 My personal website. Written in Svelte-Kit with tailwind. Deployed in Vercel
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,8 +42,10 @@
 <svg class="block w-full h-12 -mt-12" viewBox="0 0 1440 320" preserveAspectRatio="none">
   <path fill="rgb(118 81 36)" d="M0,160L48,176C96,192,192,224,288,213.3C384,203,480,149,576,144C672,139,768,181,864,181.3C960,181,1056,139,1152,122.7C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
 </svg>
-<div class="h-70 bg-[rgb(118,81,36)]">
+<div class="h-60 bg-[rgb(118,81,36)]">
+    <div class="px-6 pb-15 max-w-180 w-full mx-auto">
 
+    </div>
 </div>
 
 <svg class="block w-full h-12 -mt-12 scale-x-[-1]"  viewBox="0 0 1440 320" preserveAspectRatio="none">
